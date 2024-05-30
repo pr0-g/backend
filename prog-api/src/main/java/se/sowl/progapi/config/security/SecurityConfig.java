5df34e5ecf2f95b3797ae2054fb584a4ec85a02c
Hwasowl <sound2339@naver.com>
2024-05-30 12:27:53 +0900

    fix: SecurityConfig <-> OAuthService 순환참조 문제 해결

--- prog-api/src/main/java/se/sowl/progapi/config/security/SecurityConfig.java
+++ prog-api/src/main/java/se/sowl/progapi/config/security/SecurityConfig.java
@@ -6,15 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
-import se.sowl.progapi.oauth.service.OAuthService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final OAuthService oAuthService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,8 +27,13 @@ public class SecurityConfig {
             .logout(logout -> logout.logoutSuccessUrl("/"))
             .oauth2Login(oauth2Login -> oauth2Login
                 .defaultSuccessUrl("/oauth2/login/info", true)
-                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuthService))
+                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserService()))
             );
         return http.build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        return new DefaultOAuth2UserService();
     }
 }
