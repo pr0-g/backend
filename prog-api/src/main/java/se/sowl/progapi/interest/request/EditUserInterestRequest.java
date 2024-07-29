5e9f4edcf731935db22f585a73c51dd2fb6b7a92
owen <owenminjong@gmail.com>
2024-07-29 17:11:54 +0900

    feat: 유저 관심 분야 수정 엔드포인트 문서 추가

--- prog-api/src/main/java/se/sowl/progapi/interest/request/EditUserInterestRequest.java
+++ prog-api/src/main/java/se/sowl/progapi/interest/request/EditUserInterestRequest.java
@@ -1,18 +1,17 @@
 package se.sowl.progapi.interest.request;
 
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EditUserInterestRequest {
     @NotEmpty(message = "관심사 ID 목록은 비어있을 수 없습니다.")
     private List<Long> interestIdList;
-
-    public EditUserInterestRequest(List<Long> interestIdList) {
-        this.interestIdList = interestIdList;
-    }
 }
