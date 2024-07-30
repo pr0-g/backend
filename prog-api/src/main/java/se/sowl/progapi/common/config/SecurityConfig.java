ce4452bd903291e89eafd7a4b04290a490143ca7
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2024-07-30 15:46:45 +0900

    feat: RedisConfig 추가 (#5)

--- prog-api/src/main/java/se/sowl/progapi/common/config/SecurityConfig.java
+++ prog-api/src/main/java/se/sowl/progapi/common/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package se.sowl.progapi.common.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,10 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import se.sowl.progapi.oauth.service.OAuthService;
-import se.sowl.progdomain.user.repository.UserRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -45,9 +42,4 @@ public class SecurityConfig {
             );
         return http.build();
     }
-
-//    @Bean
-//    public DefaultOAuth2UserService oAuth2UserService() {
-//        return new DefaultOAuth2UserService();
-//    }
 }
